5e2901e4b9cdd2ce4369622ce2fc7f67f0b388bb
dbsalsgur <rkawkaos123@naver.com>
2022-11-30 13:56:24 +0900

    221130 java 예제

---
+++ javatutorial/src/org/java_exercise_01/MyClass01.java
@@ -0,0 +1,11 @@
+package org.java_exercise_01;
+
+import java.util.Scanner;
+
+public class MyClass01 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Eclipse!!");
+	}
+}
